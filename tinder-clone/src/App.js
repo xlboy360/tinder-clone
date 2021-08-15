f9007596f2910ae212c9f52a4407bb6cdac7c69b
xlboy360 <juan_jsc360@hotmail.com>
2021-08-15 10:49:56 -0500

    Creation of React app

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <Header />
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
